d0221ab740886193e01e37100ef540389af12cf5
neppord <neppord@gmail.com>
2022-04-05 14:02:24 +0200

    Added some more fine-grained steps to complete.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -13,7 +13,8 @@ data class Vec5<A>(
     val item4: A,
     val item5: A,
 ) {
-    constructor(item: A): this(item, item, item, item, item)
+    constructor(item: A) : this(item, item, item, item, item)
+
     fun <B> map(f: (A) -> B) =
         Vec5(f(item1), f(item2), f(item3), f(item4), f(item5))
 
@@ -24,6 +25,8 @@ data class Vec5<A>(
         f.item4(item4),
         f.item5(item5)
     )
+
+    fun asList() = listOf(item1, item2, item3, item4, item5)
 }
 
 fun yellow(char: Char) = Cell(Color.YELLOW, char)
@@ -31,10 +34,16 @@ fun grey(char: Char) = Cell(Color.GREY, char)
 fun green(char: Char) = Cell(Color.GREEN, char)
 
 // Filter rules for Wordle
-fun mustContain(char: Char): (Char) -> Boolean = TODO()
-fun mustNotContain(char: Char): (Char) -> Boolean = TODO()
-fun atLeast(clue: Vec5<Cell>): (Vec5<Char>) -> Boolean = TODO()
-fun lessThan(clue: Vec5<Cell>): (Vec5<Char>) -> Boolean = TODO()
+fun mustContain(char: Char): (Char) -> Boolean =  TODO()
+fun mustNotContain(char: Char): (Char) -> Boolean =  TODO()
+val checkCellConstraintForCell: (Cell) -> (Char) -> Boolean = TODO()
+val checkCellConstraintsForWord: (Vec5<Cell>) -> (Vec5<Char>) -> Boolean =
+    TODO()
+
+fun atLeast(char: Char, amount: Int): (Vec5<Char>) -> Boolean = TODO()
+fun lessThan(char: Char, amount: Int): (Vec5<Char>) -> Boolean = TODO()
+
+fun checkAmountConstraint(clue: Vec5<Cell>): (Vec5<Char>) -> Boolean = TODO()
 
 // utility method
 fun <A> ((A) -> Boolean).and(other: (A) -> Boolean): (A) -> Boolean = TODO()
