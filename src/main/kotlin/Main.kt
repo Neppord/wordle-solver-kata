fb38ffd7ea56b5ebce8d3e4325d531f8d8ede3af
neppord <neppord@gmail.com>
2022-04-26 15:32:25 +0200

    Implement a main method that uses the current rule set

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -11,7 +11,7 @@ fun checkCellConstraintsForWord(clue: Vec5<Cell>): (Vec5<Char>) -> Boolean =
         val cellConstraints: Vec5<(Char) -> Boolean> =
             clue.map(Cell::checkCellConstraint)
         val apply: Vec5<Boolean> = word.apply(cellConstraints)
-        apply.reduce { a, b -> a && b }
+        apply.reduce(Boolean::and)
     }
 
 // atLeast('S', 1)(Vec5('S', 'L', 'A', 'T', 'E')) == true
@@ -39,12 +39,39 @@ fun yellow(char: Char) = Cell(Color.YELLOW, char)
 fun grey(char: Char) = Cell(Color.GREY, char)
 fun green(char: Char) = Cell(Color.GREEN, char)
 
+
+typealias Clue = Vec5<Cell>
+
+val makeRuleFromClue: (clue: Clue) -> (Vec5<Char>) -> Boolean =
+    ::checkCellConstraintsForWord
+
+fun makeRulesFromClues(clues: List<Clue>): (Vec5<Char>) -> Boolean = clues
+    .map(makeRuleFromClue)
+    .reduce { a, b -> a.plus(b) }
+
+val clues: List<Clue> = listOf(
+    Vec5(yellow('S'), grey('L'), grey('A'), grey('T'), yellow('E')),
+)
+
 fun main(args: Array<String>) {
     val words: List<Vec5<Char>> = File("words.txt")
         .readLines()
+        .map(String::uppercase)
         .map {
             Vec5(it[0], it[1], it[2], it[3], it[4])
         }
-    val clue =
-        Vec5(grey('S'), yellow('L'), yellow('A'), yellow('T'), grey('E'))
+
+
+    val rules: (Vec5<Char>) -> Boolean = makeRulesFromClues(clues)
+    val checkedWords: List<Vec5<Char>> = words.filter(rules)
+    val wordsToPrint: List<String> = checkedWords
+        .shuffled()
+        .take(5)
+        .map {
+            it.map(Char::toString).reduce(String::plus)
+        }
+    for (word in wordsToPrint) {
+        println(word)
+    }
+    println("""words left: ${checkedWords.count()}""")
 }
